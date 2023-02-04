b65395d8c6fb8a362bd907d05fe65112e614b08d
Sebastian Roslon <sebekk7@gmail.com>
2023-02-04 13:42:35 +0100

    add mySql connetion add -dev -prod profiles

--- src/main/java/pl/roslon/ToDoList/controller/ItemController.java
+++ src/main/java/pl/roslon/ToDoList/controller/ItemController.java
@@ -1,17 +1,23 @@
 package pl.roslon.ToDoList.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import pl.roslon.ToDoList.entities.Item;
 import pl.roslon.ToDoList.repository.ItemRepository;
 
 import javax.validation.Valid;
+import java.net.URI;
 
+//@RestController
 @Controller
 public class ItemController {
 
@@ -29,6 +35,16 @@ public class ItemController {
         return "index";
     }
 
+//    @GetMapping("/index")
+//    public ResponseEntity<Void> save(Model model, Item item) {
+//        model.addAttribute("item", item);
+//        model.addAttribute("items", itemRepository.findAll());
+//        return new ResponseEntity<Void>(HttpStatus.OK);
+//    }
+
+
+
+
     @PostMapping("/save")
     public String addItem(@Valid Item item, BindingResult result) {
         if (result.hasErrors()) {
@@ -37,6 +53,23 @@ public class ItemController {
         itemRepository.save(item);
         return "redirect:/index";
     }
+
+//    @PostMapping("/save")
+//    public ResponseEntity<String> addItem(@Valid Item item, BindingResult result) {
+//
+//        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+//                .path("/save")
+//                .buildAndExpand()
+//                .toUri();
+//
+//        if (result.hasErrors()) {
+//           return ResponseEntity.created(location).body("index");
+//        }
+//        itemRepository.save(item);
+//        return ResponseEntity.created(location).body("index");
+//    }
+
+
 
     @GetMapping("/delete/{id}")
     public String deleteItem(@PathVariable("id") Item item) {
